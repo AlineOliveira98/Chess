00c59c959bde38f11c2f22b1b48180f92b6605f7
Aline Oliveira <alineboliveira98@gmail.com>
2025-05-20 19:45:38 -0300

    bishop movement

--- Assets/_Sources/Board/Scripts/Pieces/Bishop.cs
+++ Assets/_Sources/Board/Scripts/Pieces/Bishop.cs
@@ -7,6 +7,34 @@ public class Bishop : Piece
     {
         var possibleMoves = new List<Vector2Int>();
 
+        Vector2Int[] directions = {
+            new Vector2Int(1, 1),
+            new Vector2Int(1, -1),
+            new Vector2Int(-1, 1),
+            new Vector2Int(-1, -1),
+        };
+
+        foreach (var dir in directions)
+        {
+            var pos = Coordinate;
+            while (true)
+            {
+                pos += dir;
+
+                if (!IsInsideBoard(pos)) break;
+
+                if (!TileIsEmpty(pos))
+                {
+                    if (TileHasOpponentPiece(pos))
+                        possibleMoves.Add(pos);
+
+                    break;
+                }
+
+                possibleMoves.Add(pos);
+            }
+        }
+
         return possibleMoves;
     }
 }
